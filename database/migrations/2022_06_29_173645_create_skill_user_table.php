38e8361156d92b6c1572b427fd0dda0b7e2a1481
Mohamed <nrookman_27@yahoo.com>
2022-06-29 19:55:10 +0200

    Add migration files

---
+++ database/migrations/2022_06_29_173645_create_skill_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skill_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('skill_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->unique(['skill_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skill_user');
+    }
+};
